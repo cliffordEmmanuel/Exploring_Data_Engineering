6305bd14cd01cea654b65a3c2a57d9a5c22edd22
cliffordEmmanuel <cliffemmanuels@gmail.com>
2019-10-08 12:04:45 +0000

    task completed

---
+++ project1/s3.py
@@ -0,0 +1,30 @@
+
+import boto3
+import pandas as pd
+
+
+def main():
+    # initializing the s3 resource
+    s3 = boto3.client('s3')
+    bucket = 'blossom-data-eng-clifford'
+
+    # downloading the file from s3 bucket
+    s3.download_file('blossom-data-engs', 'free-7-million-company-dataset.zip', 'company_data.zip')
+
+    # reading the file using pandas
+    data = pd.read_csv('company_data.zip', index_col=0, compression='gzip')
+
+    # removing the rows that have null values in the domain column...
+    data.dropna(subset=['domain'], inplace=True)
+
+    # writing the output files
+    data.to_parquet('free-7-million-company-dataset.parquet')
+    data.to_json('free-7-million-company-dataset-json.gzip', compression='gzip')
+
+    # now on to using the uploading to the s3 bucket
+    s3.upload_file('free-7-million-company-dataset.parquet', bucket, 'free-7-million-company-dataset.parquet')
+    s3.upload_file('free-7-million-company-dataset-json.gzip', bucket, 'free-7-million-company-dataset-json.gzip')
+
+
+if __name__ == "__main__":
+    main()
