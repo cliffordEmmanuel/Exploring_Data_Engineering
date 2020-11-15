a11c5cb43868e2bbf5de138aa352bce40cd0dd9c
cliffordEmmanuel <cliffemmanuels@gmail.com>
2020-11-15 07:29:30 +0000

    extracts all the required tweet attributes

--- pipelining_for_tweets_sentiment_analysis/twitter_scraper.py
+++ pipelining_for_tweets_sentiment_analysis/twitter_scraper.py
@@ -53,6 +53,15 @@ tweet_source_list = []
 tweet_coordinate_list = []
 tweet_retweet_count_list = []
 tweet_likes_count_list = []
+user_id_list = []
+user_name_list = []
+user_screen_name_list = []
+user_location_list = []
+user_followers_count_list = []
+user_friends_count_list = []
+user_created_at_list = []
+user_is_verified_list = []
+user_description_list = []
 
 
 for tweet in tweets_list:
@@ -63,6 +72,15 @@ for tweet in tweets_list:
     tweet_coordinate_list.append(tweet.coordinates)
     tweet_retweet_count_list.append(tweet.retweet_count)
     tweet_likes_count_list.append(tweet.favorite_count)
+    user_id_list.append(tweet.user.id_str)
+    user_name_list.append(tweet.user.name)
+    user_screen_name_list.append(tweet.user.screen_name)
+    user_location_list.append(tweet.user.location)
+    user_followers_count_list.append(tweet.user.followers_count)
+    user_friends_count_list.append(tweet.user.friends_count)
+    user_created_at_list.append(tweet.user.created_at)
+    user_is_verified_list.append(tweet.user.verified)
+    user_description_list.append(tweet.user.description)
 
 tweets_df = pd.DataFrame( {
     "tweet_created_at": tweet_created_at_list,
@@ -71,6 +89,16 @@ tweets_df = pd.DataFrame( {
     "tweet_source" : tweet_source_list,
     "tweet_coordinate" : tweet_coordinate_list,
     "tweet_retweet_count" : tweet_retweet_count_list,
-    "tweet_likes_count" : tweet_likes_count_list
+    "tweet_likes_count" : tweet_likes_count_list,
+    "user_id_list" : user_id_list,
+    "user_name_list": user_name_list,
+    "user_screen_name_list": user_screen_name_list,
+    "user_location_list": user_location_list,
+    "user_followers_count_list": user_followers_count_list,
+    "user_friends_count_list": user_friends_count_list,
+    "user_created_at_list": user_created_at_list,
+    "user_is_verified_list": user_is_verified_list,
+    "user_description_list": user_description_list
 })
 
+print(tweets_df.head(10))
