20df5f73c332323960f0bad893c543baeaa7f624
cliffordEmmanuel <cliffemmanuels@gmail.com>
2020-11-30 05:54:39 +0000

    script completed

--- pipelining_for_tweets_sentiment_analysis/twitter_scraper.py
+++ pipelining_for_tweets_sentiment_analysis/twitter_scraper.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import tweepy
 import pandas as pd
 import time
@@ -15,21 +16,29 @@ api = tweepy.API(auth)
 # writing a method to get tweets by a search key
 search_key = "endsars -filter:retweets"  # to filter retweets...
 
-# working with the standard api rate limitations
-# you can make ~ 900 api calls every 15 minutes.
-# creating a scrape tweets function
 
-def scrapeTweets(search_key, number_of_tweets, number_of_calls):
-    """"downloads a specified number of tweets based on the search key by making a specified number of calls to the standard twitter api endpoint."""
+
+def scrapeTweets(search_key, number_of_tweets, number_of_requests):
+    """"downloads a specified number of tweets based on the search key by making a specified number of calls to the standard twitter  search api endpoint."""
     
-    print("Creating a dataframe to store tweets....")
     tweets_df = pd.DataFrame([])
+    tweets_counter = 0
+    # working with the standard api rate limitations
+    # you can make up to 450 requests in a 15 minute time window.
+    # a single request returns up to 100 tweets
 
-
-    for call in range(number_of_calls):
+    run_start = time.time()
+    for request in range(number_of_requests):
         # this returns tweets bundled into a iterator object
-        print(f"Making API call number: {call+1}...")
-        tweets = tweepy.Cursor(api.search, q=search_key,lang="en").items(number_of_tweets)
+        call_start_time = time.time()
+        tweets = tweepy.Cursor( api.search, 
+                                q=search_key,
+                                lang="en", 
+                                count=100, 
+                                wait_on_rate_limit=True, 
+                                wait_on_rate_limit_notify=True,
+                                until='2020-11-28').items(number_of_tweets)
+        
         tweets_list = [tweet for tweet in tweets]
 
         tweet_created_at_list = []
@@ -51,6 +60,7 @@ def scrapeTweets(search_key, number_of_tweets, number_of_calls):
 
 
         for tweet in tweets_list:
+            tweets_counter += 1
             tweet_created_at_list.append(tweet.created_at)
             tweet_id_list.append(tweet.id_str)
             tweet_text_list.append(tweet.text)
@@ -87,18 +97,26 @@ def scrapeTweets(search_key, number_of_tweets, number_of_calls):
             "user_is_verified_list": user_is_verified_list,
             "user_description_list": user_description_list
         })
-
+        call_end_time = time.time()
         tweets_df = pd.concat([tweets_df,single_run_df])
+        print(f"{number_of_tweets} tweets downloaded in request {request+1} in {round(call_end_time-call_start_time)} seconds")
 
-        print("Waiting for 2 mins to make the next call...")
-        time.sleep(120) # wait for ~ 15min before the next api call
+    
+    # wait for ~ 15min before the next api run
+    run_end = time.time()
+    print(f"*******************************************************")
+    print(f"Run took {round((run_end-run_start)/60,2)} mins to download {tweets_counter} tweets!!")
+    print(f"*******************************************************")
+    print("Waiting for 15 mins to make the next run...")
+    # time.sleep(920) 
 
     return tweets_df
 
+result = scrapeTweets(search_key,100,5)
 
-
-
-result = scrapeTweets(search_key,10,6)
-
-result.to_csv("test.csv")
+print("Saving data...")
+filename = f'tweets_downloaded_{time.strftime("%Y%m%d_%H%M%S",time.localtime())}.csv'
+result.to_csv(filename)
 print("data saved!!...")
+
+print(result.tweet_id.value_counts())
